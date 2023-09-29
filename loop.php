b367458fb8bd2f8e112a3dc8292750abe8d6a05f
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-09-29 15:20:39 -0300

    co

---
+++ loop.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    //variaveis
+    $num = 4;
+    $limite = 30;
+
+    while ($limite <= 30);
+
+    $num = 4; // Inicializa o contador como 4
+    
+    for ($i = 1; $i <= 30; $i++) {
+        // Verifica se o contador atingiu 24
+        if ($contador == 24) {
+            break;
+        }
+        
+        echo "Contador: $contador<br>";
+    
+        // Incrementa o contador em 2
+        $contador += 2;
+    }
+    
+    ?>
+    
+</body>
+</html>
