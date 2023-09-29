5bacd89e0ebd70fca55823d2823cbffc91012a62
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-09-29 15:25:13 -0300

    commit bia

--- exerc.php
+++ exerc.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Loop</title>
 </head>
 <body>
     <?php
@@ -14,17 +14,17 @@
 
     while ($limite <= 30);
 
-    $num = 4; // Inicializa o contador como 4
+    $num = 4; //inicia o contador 4
     
     for ($i = 1; $i <= 30; $i++) {
-        // Verifica se o contador atingiu 24
+        //verifica se o contador atingiu 24
         if ($contador == 24) {
             break;
         }
         
         echo "Contador: $contador<br>";
     
-        // Incrementa o contador em 2
+        //contador em 2
         $contador += 2;
     }
     
